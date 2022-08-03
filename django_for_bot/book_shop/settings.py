c83fdcc07d1493cafa70403a9424ffc844f5958c
v1ntex <leonidt668@gmail.com>
2022-08-03 13:50:11 +0300

    added AUTH_USER_MODEL

--- django_for_bot/book_shop/settings.py
+++ django_for_bot/book_shop/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+AUTH_USER_MODEL = 'users.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
