2831dca5129957d0036db09cb602a7149db4dfc4
v1ntex <leonidt668@gmail.com>
2022-08-03 13:49:21 +0300

    registered models

--- django_for_bot/users/admin.py
+++ django_for_bot/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    list_display = ('username', 'telegram_id', 'is_staff', 'is_active', 'date_joined',)
+    search_fields = ('username',)
+    ordering = ('username',)
