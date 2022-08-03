3568002beda9569ee8a08bf6e2b4e63a20fdb7f8
v1ntex <leonidt668@gmail.com>
2022-08-03 13:49:32 +0300

    created models

--- django_for_bot/users/models.py
+++ django_for_bot/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    telegram_id = models.PositiveIntegerField(verbose_name='Telegram user id', unique=True, null=False)
+    REQUIRED_FIELDS = ['telegram_id', 'username']
+
+    def __str__(self):
+        return f'#{self.telegram_id} | {self.username}'
