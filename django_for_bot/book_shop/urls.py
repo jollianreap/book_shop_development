d17be54816dc9b6c5041b7a8923532ccb32d836c
v1ntex <leonidt668@gmail.com>
2022-08-14 12:40:37 +0300

    added new namespace: users

--- django_for_bot/book_shop/urls.py
+++ django_for_bot/book_shop/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("products.urls", namespace="products"))
+    path("api/", include("products.urls", namespace="products")),
+    path('apiv2/', include("users.urls", namespace="users")),
 ]
