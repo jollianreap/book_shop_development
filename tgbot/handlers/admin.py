4e7ed951f4e37a67266cc066f8a349d3ce946786
v1ntex <leonidt668@gmail.com>
2022-08-14 14:19:21 +0300

    connected telegram bot with django: added a authentication`s checking and ability to create new users

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,9 +1,10 @@
 from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ParseMode
 
 
 async def admin_start(message: Message):
-    await message.reply("Hello, admin!")
+    await message.reply("Hello, admin!\n"
+                        "Вот тебе ссылочка на админ панель(пароль и логин знаешь): <a href='http://127.0.0.1:8000/admin'> Админ Панель</a>", parse_mode=ParseMode.HTML)
 
 
 def register_admin(dp: Dispatcher):
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,10 +1,25 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
+from ..keyboards.reply import main_keyboard
+import requests
 
 
 async def user_start(message: Message):
-    await message.reply("Hello, user!")
+    await message.reply(f"Привет, {message.from_user.username}!\n"
+                        f"Щас проверю есть ли ты в базе")
+    response = requests.get(f'http://127.0.0.1:8000/apiv2/check_user/{message.from_user.id}')
+    if response.status_code == 200:
+        await message.reply("Отлично, ты есть в базе", reply_markup=main_keyboard)
+    elif response.status_code == 500:
+        await message.reply("Тебя нету в базе, но это не проблема, щас создам тебе аккаунт, секундочку...")
+        create = requests.post("http://127.0.0.1:8000/apiv2/create_user/", data={"telegram_id": message.from_user.id,
+                                                                                 "username": message.from_user.username})
+        if create.status_code == 200 or create.status_code == 201:
+            await message.reply("Добро пожаловать в семью "
+                                , reply_markup=main_keyboard)
 
+        else:
+            await message.reply(f"Проблемка тут, попробуй ещё раз")
 
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
