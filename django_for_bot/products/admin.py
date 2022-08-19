575724324a6e7c5223538591509cd1d7db2f3e5a
v1ntex <leonidt668@gmail.com>
2022-08-19 19:04:42 +0300

    changed display in admin panel

--- django_for_bot/products/admin.py
+++ django_for_bot/products/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('author_name', 'product_name', 'category', 'description',
+                    'price', 'quantity')
+    list_display = ('author_name', 'product_name', 'category')
+    ordering = ('product_name',)
+    search_fields = ('author_name', 'product_name')
+
